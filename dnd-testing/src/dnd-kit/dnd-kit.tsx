9fd93543ac016ef95fc6356a225a2e3cf1a8c15d
Cîrstoiu Bogdan-Florin <bogdan.is.a.beast@gmail.com>
2022-09-27 11:47:49 +0000

    Update 2 files

--- dnd-testing/src/dnd-kit/dnd-kit.tsx
+++ dnd-testing/src/dnd-kit/dnd-kit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   DndContext,
   closestCenter,
@@ -7,10 +7,8 @@ import {
   useSensor,
   useSensors,
   DragEndEvent,
-  DragOverlay,
 } from "@dnd-kit/core";
 import {
-  arrayMove,
   SortableContext,
   sortableKeyboardCoordinates,
   verticalListSortingStrategy,
@@ -19,7 +17,11 @@ import { SortableItem } from "./sortable";
 
 export default function DndKit() {
   const [items, setItems] = useState([...Array(50).keys()]);
-  const [isDragging, setIsDragging] = useState(false);
+
+  useEffect(() => {
+    setItems((items) => items.map((item) => item + 1));
+  }, []);
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -39,14 +41,12 @@ export default function DndKit() {
         const oldIndex = items.indexOf(active.id as number);
         const newIndex = items.indexOf(over.id as number);
 
-        return arrayMove(items, oldIndex, newIndex);
+        const arr = items;
+        const itemToMove = arr.splice(oldIndex, 1)[0];
+        arr.splice(newIndex, 0, itemToMove);
+        return [...arr];
       });
     }
-    setIsDragging(false);
-  }
-
-  function handleDragStart() {
-    setIsDragging(true);
   }
 
   return (
@@ -54,24 +54,11 @@ export default function DndKit() {
       sensors={sensors}
       collisionDetection={closestCenter}
       onDragEnd={handleDragEnd}
-      onDragStart={handleDragStart}
     >
       <SortableContext items={items} strategy={verticalListSortingStrategy}>
         {items.map((id) => (
-          <SortableItem key={id} id={id} content={id} />
+          <SortableItem key={id} id={id} content={id - 1} />
         ))}
-        <DragOverlay>
-          {isDragging && (
-            <div
-              style={{
-                height: "4px",
-                zIndex: 999999,
-                background: "lightblue",
-                width: "100%",
-              }}
-            />
-          )}
-        </DragOverlay>
       </SortableContext>
     </DndContext>
   );
